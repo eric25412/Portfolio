e481ac6e648f25cf2d2a62e1698e739aa00630b5
eric lopez <ericlz.25412@gmail.com>
2019-09-02 23:51:56 -0700

    work history

--- public/resume.js
+++ public/resume.js
@@ -10,13 +10,30 @@
 
 // });
 
-var button = document.getElementById('resume-button')
-button.addEventListener("click", function() {
-    console.log("done")
-	document.querySelector('.dark-theme').style.display = "flex";
-});
+// var button = document.getElementById('resume-button')
+// button.addEventListener("click", function() {
+//     console.log("done")
+//     // document.querySelector('.dark-theme').style.display = "flex";
+//     document.documentElement('<img src="img/offical-resume.jpg"/>');
+//     return false;
+// });
 
-var close=document.querySelector('.close')
-close.addEventListener("click", function() {
-    document.querySelector('.dark-theme').style.display = "none";
-});
+// var close=document.querySelector('.close')
+// close.addEventListener("click", function() {
+//     document.querySelector('.dark-theme').style.display = "none";
+// });
+
+$(document).ready(function() {
+
+    // Here we use jQuery to select the header with "click-me" as its ID.
+    // Notice I have the #click-me, click, and then the function
+    // So $("#id|.class|element").on("action", function(){});
+    // And so whenever it is clicked...
+    $("#resume-button").on("click", function() {
+        window.location = 'img/offical-resume-converted-pdf.pdf';
+    });
+
+  });
+
+
+
